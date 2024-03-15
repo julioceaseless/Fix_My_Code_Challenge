c1b8bbe410c4d07e810dbe19908432f1a3c3d94b
julioceaseless <julioceaseless@gmail.com>
2024-03-15 12:28:20 +0000

    make it accept only one parameter

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -30,7 +30,7 @@ class Square():
 
 if __name__ == "__main__":
     """ run when called directly"""
-    s = Square(width=12, height=9)
+    s = Square(width=12)
     print(s)
     print(s.area_of_my_square())
     print(s.perimeter_of_my_square())
